b5aad32c63afb728ca1a41e324863a1859991e06
Marian Andrecki <marian.andrecki@gmail.com>
2017-07-18 20:26:13 +0100

    Added module and heads building, loading, saving.

--- hydranet.py
+++ hydranet.py
@@ -11,43 +11,148 @@ from keras.layers import Input, Dense, Convolution2D, Deconvolution2D, MaxPoolin
     UpSampling2D, Merge, LSTM, Flatten, ZeroPadding2D, Reshape, BatchNormalization, Dropout
 from keras.layers.wrappers import TimeDistributed
 
-import network_structure
+from keras.utils.visualize_util import plot as draw_network
+
+import numpy as np
+from tqdm import tqdm
+
+import tensorflow as tf
+
+from structured_container import DataContainer
+
+FOLDER_DIAGRAMS = 'diagrams'
+FOLDER_MODELS = 'models'
+
+IM_WIDTH = 28
+IM_HEIGHT = 28
+IM_CHANNELS = 1
+IM_SHAPE = (IM_WIDTH, IM_HEIGHT, IM_CHANNELS)
+
+SERIES_SHIFT = 1
+EP_LEN = 100 - SERIES_SHIFT
+
+V_SIZE = 16
+
+BATCH_SIZE = 32
+
+DEFAULT_SCHEME = {
+    'clear_percepts': 4,
+    'mixed_percepts': 8,
+}
 
 
 class HydraNet(object):
     def __init__(self, **kwargs):
-        # self.models_folder = kwargs.get('models_folder', MODELS_FOLDER)
+        self.training_scheme = kwargs.get('training_scheme', DEFAULT_SCHEME)
+        self.v_size = kwargs.get('v_size', V_SIZE)
 
-        self.structure = kwargs.get('structure', network_params.DEFAULT_STRUCTURE)
-
-        self.pred_ahead = True
-
-        # branches of network
+        # modules of network
         self.encoder = None
         self.decoder = None
-        self.generator = None
-        self.state_predictor = None
-        self.action_mapper = None
-        self.action_predictor = None
-        self.state_sampler = None
+        self.lstm_train = None
+        self.lstm_replay = None
 
-        self.discriminator = None
-        self.screen_discriminator = None
-        self.state_discriminator = None
+        # lstm_replay purposely skipped (weights are copied from lstm_train
+        self.modules = [self.encoder, self.decoder, self.lstm_train]
+
+        # self.state_sampler = None
 
         # full networks
-        self.autoencoder = None
-        self.autoencoder_gen = None
-        self.autoencoder_disc = None
-        self.autoencoder_gan = None
+        self.pred_ae = None
+        self.stepper = None
 
-        self.screen_predictor = None
-        self.screen_predictor_g = None
-        self.screen_predictor_d = None
+        # data containers
+        self.train_box = DataContainer('data-balls/balls-train.pt', batch_size=32, ep_len_read=EP_LEN)
+        self.test_box = DataContainer('data-balls/balls-valid.pt', batch_size=32, ep_len_read=EP_LEN)
 
-        self.state_assigner = None
-        self.future_sampler_g = None
-        self.future_sampler_d = None
+        # initialisation
+        self.build_modules()
+        # self.load_modules()
+        self.build_heads()
 
-        self.build_branches()
-        self.build_networks()
+    def build_modules(self):
+        # build encoder
+        input_im = Input(shape=IM_SHAPE)
+        h = Convolution2D(16, 5, 5, subsample=(2, 2), activation='relu', border_mode='same')(input_im)
+        h = Convolution2D(8, 3, 3, subsample=(2, 2), activation='relu', border_mode='same')(h)
+        h = Reshape((392,))(h)
+        v = Dense(self.v_size, activation='relu')(h)
+        m = Model(input_im, v, name='encoder')
+
+        draw_network(m, to_file='{0}/{1}.png'.format(FOLDER_DIAGRAMS, m.name))
+        m.summary()
+        self.encoder = m
+
+        # build decoder
+        input_v = Input(shape=(self.v_size,))
+        h = Dense(8 * 7 * 7, activation='relu')(input_v)
+        h = Reshape((7, 7, 8))(h)
+        h = UpSampling2D((2, 2))(h)
+        h = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(h)
+        h = UpSampling2D((2, 2))(h)
+        output_im = Convolution2D(1, 5, 5, activation='sigmoid', border_mode='same')(h)
+        m = Model(input_v, output_im, name='decoder')
+
+        draw_network(m, to_file='{0}/{1}.png'.format(FOLDER_DIAGRAMS, m.name))
+        m.summary()
+        self.decoder = m
+
+        # lstm for training
+        self.lstm_train = LSTM(V_SIZE, return_sequences=True)
+
+        # lstm for replays
+        self.lstm_replay = LSTM(V_SIZE, stateful=True)
+
+        # TODO: aux variables: loss, position, velocity
+        # TODO: generator (decoder with noise)
+
+    def load_modules(self, tag='0'):
+        for module in self.modules:
+            fpath = '{}/{}-{}.hdf5'.format(FOLDER_MODELS, module.name, tag)
+            module.load_weights(fpath)
+
+        self.lstm_replay.set_weights(self.lstm_train.get_weights())
+
+    def save_modules(self, tag='0'):
+        for module in self.modules:
+            fpath = '{}/{}-{}.hdf5'.format(FOLDER_MODELS, module.name, tag)
+            module.save_weights(fpath)
+
+    def build_heads(self):
+        # build predictive autoencoder
+        input_ims = Input(shape=(EP_LEN - SERIES_SHIFT, IM_WIDTH, IM_HEIGHT, IM_CHANNELS))
+        td1 = TimeDistributed(self.encoder,
+                                input_shape=(EP_LEN - SERIES_SHIFT,
+                                             IM_WIDTH,
+                                             IM_HEIGHT,
+                                             IM_CHANNELS))
+
+        h = td1(input_ims)
+        h = self.lstm_train(h)
+        td2 = TimeDistributed(self.decoder, input_shape=((EP_LEN, V_SIZE)))
+        output_preds = td2(h)
+        m = Model(input_ims, output_preds, name='pred_ae_train')
+        draw_network(m, to_file='{0}/{1}.png'.format(FOLDER_DIAGRAMS, m.name))
+        m.summary()
+        m.compile(optimizer=Adam(lr=0.001), loss='mse')
+        self.pred_ae = m
+
+        # build replayer
+        input_im = Input(batch_shape=(1, IM_WIDTH, IM_HEIGHT, IM_CHANNELS))
+
+        h = self.encoder(input_im)
+        h = Reshape((1, self.v_size))(h)
+        h = self.lstm_replay(h)
+        output_recon = self.decoder(h)
+        m = Model(input_im, output_recon, name='stepper')
+        draw_network(m, to_file='{0}/{1}.png'.format(FOLDER_DIAGRAMS, m.name))
+        m.summary()
+        self.stepper = m
+
+    def train_pred_ae(self, n_batches=100, ):
+
+if __name__ == '__main__':
+    hydra = HydraNet()
+
+
+

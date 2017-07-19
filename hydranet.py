b7567302edf73127bb47d81902cda3ec14e627f1
Marian Andrecki <marian.andrecki@gmail.com>
2017-07-19 18:20:55 +0100

    Added gif prediction generation and loss plots.

--- hydranet.py
+++ hydranet.py
@@ -10,19 +10,25 @@ from keras.optimizers import Adam, Adadelta, RMSprop, SGD
 from keras.layers import Input, Dense, Convolution2D, Deconvolution2D, MaxPooling2D,\
     UpSampling2D, Merge, LSTM, Flatten, ZeroPadding2D, Reshape, BatchNormalization, Dropout
 from keras.layers.wrappers import TimeDistributed
-
 from keras.utils.visualize_util import plot as draw_network
-
-import numpy as np
-from tqdm import tqdm, trange
-import time
 
 import tensorflow as tf
 
+from PIL import Image, ImageFont, ImageDraw
+import matplotlib.pyplot as plt
+import PIL
+import numpy as np
+import time
+import imageio
+from tqdm import tqdm, trange
+
 from structured_container import DataContainer
+from particle_filter import ParticleFilter
 
 FOLDER_DIAGRAMS = 'diagrams'
 FOLDER_MODELS = 'models'
+FOLDER_PLOTS = 'plots'
+HEADER_HEIGHT = 18
 
 IM_WIDTH = 28
 IM_HEIGHT = 28
@@ -32,37 +38,52 @@ IM_SHAPE = (IM_WIDTH, IM_HEIGHT, IM_CHANNELS)
 SERIES_SHIFT = 1
 EP_LEN = 100 - SERIES_SHIFT
 
-V_SIZE = 16
+V_SIZE = 64
 
 BATCH_SIZE = 32
 TEST_EVERY_N_BATCHES = 10
 
 DEFAULT_SCHEME = {
     'clear_training': True,  # run training on clear episodes (non-masked percepts)
-    'clear_training_batches': 500,  # how many batches?
+    # 'clear_training': False,  # run training on clear episodes (non-masked percepts)
+    'clear_batches': 500,  # how many batches?
+    'lr_initial': 0.001,
     'guaranteed_percepts': 4,  # how many first percepts are guaranteed to be non-masked?
     'uncertain_percepts': 8,  # how many further have a high chance to be non-masked?
     'p_levels': np.sqrt(np.linspace(0.05, 0.99, 10)).tolist(),  # progressing probabilities of masking percepts
-    'p_level_batches': 200,  # how many batches per level
+    # 'p_levels': [],  # progressing probabilities of masking percepts
+    'p_level_batches': 400,  # how many batches per level
     'p_final': 0.99,  # final probability level
     'lr_final': 0.0002,
-    'final_batches': 2000,  # number of batches for final training
+    'final_batches': 4000,  # number of batches for final training
 }
+
+
+def create_im_label(label):
+    font = ImageFont.truetype("/usr/share/fonts/dejavu/DejaVuSans.ttf", HEADER_HEIGHT)
+    im = Image.new('F', (IM_WIDTH, HEADER_HEIGHT), 1.0)
+    draw = ImageDraw.Draw(im)
+    draw.text((1, 0), label, 0.0, font)
+
+    return np.array(im)
 
 
 class HydraNet(object):
     def __init__(self, **kwargs):
         # training configuration
-        self.training_scheme = kwargs.get('training_scheme', DEFAULT_SCHEME)
-        self.clear_training = self.training_scheme.get('clear_training', True)
-        self.clear_training_batches = self.training_scheme.get('clear_training_batches', 500)
-        self.guaranteed_percepts = self.training_scheme.get('guaranteed_percepts', 4)
-        self.uncertain_percepts = self.training_scheme.get('uncertain_percepts', 8)
-        self.p_levels = self.training_scheme.get('p_levels', [])
-        self.p_level_batches = self.training_scheme.get('p_level_batches', 200)
-        self.p_final = self.training_scheme.get('p_final', None)
-        self.lr_final_ = self.training_scheme.get('lr_final', 0.0002)
-        self.final_batches = self.training_scheme.get('final_batches', 1000)
+        self.training_scheme = DEFAULT_SCHEME
+        self.training_scheme.update(kwargs)
+
+        self.clear_training = self.training_scheme.get('clear_training')
+        self.clear_batches = self.training_scheme.get('clear_batches')
+        self.lr_initial = self.training_scheme.get('lr_initial')
+        self.guaranteed_percepts = self.training_scheme.get('guaranteed_percepts')
+        self.uncertain_percepts = self.training_scheme.get('uncertain_percepts')
+        self.p_levels = self.training_scheme.get('p_levels')
+        self.p_level_batches = self.training_scheme.get('p_level_batches')
+        self.p_final = self.training_scheme.get('p_final')
+        self.lr_final = self.training_scheme.get('lr_final')
+        self.final_batches = self.training_scheme.get('final_batches')
 
         # loss trackers
         self.pred_loss_train = []
@@ -136,6 +157,7 @@ class HydraNet(object):
     def load_modules(self, folder=FOLDER_MODELS, tag='0'):
         for module in self.modules:
             fpath = '{}/{}-{}.hdf5'.format(folder, module.name, tag)
+            print('Loading {} from {}'.format(module.name, fpath))
             module.load_weights(fpath)
 
         self.lstm_replay.set_weights(self.state_pred_train.get_weights())
@@ -143,7 +165,12 @@ class HydraNet(object):
     def save_modules(self, folder=FOLDER_MODELS, tag='0'):
         for module in self.modules:
             fpath = '{}/{}-{}.hdf5'.format(folder, module.name, tag)
+            print('Saving {} to {}'.format(module.name, fpath))
             module.save_weights(fpath)
+
+    def load_model(self, fpath):
+        self.pred_ae.load_weights(fpath)
+        self.lstm_replay.set_weights(self.state_pred_train.get_weights())
 
     def build_heads(self):
         # build predictive autoencoder
@@ -182,8 +209,8 @@ class HydraNet(object):
         for_removal = np.random.random(EP_LEN) < p
         clear_percepts = self.guaranteed_percepts + np.random.randint(0, self.uncertain_percepts)
         for_removal[0:clear_percepts] = False
-        images_masked[:, for_removal] = 0
-        return images
+        images_masked[:, for_removal, ...] = 0
+        return images_masked
 
     def train_pred_ae(self, image_getter, p=0.0, test=False):
         images = image_getter()
@@ -201,11 +228,13 @@ class HydraNet(object):
         return loss
 
     def execute_scheme(self, train_getter, test_getter):
+        self.pred_ae.compile(optimizer=Adam(lr=self.lr_initial), loss='mse')
+
         postfix = {}
         if self.clear_training:
             current_p = 0.0
             print('Current p:', current_p)
-            bar = trange(self.p_level_batches)
+            bar = trange(self.clear_batches)
             for i in bar:
                 self.pred_loss_train.append(self.train_pred_ae(train_getter, p=current_p))
                 smooth_loss = np.mean(self.pred_loss_train[-10:])
@@ -218,7 +247,7 @@ class HydraNet(object):
 
                 bar.set_postfix(**postfix)
 
-        self.save_modules()
+            self.save_modules()
 
         while len(self.p_levels) > 0:
             current_p = self.p_levels.pop(0)
@@ -237,12 +266,12 @@ class HydraNet(object):
 
                 bar.set_postfix(**postfix)
 
-        self.save_modules()
-
         if self.p_final is not None:
+            self.pred_ae.compile(optimizer=Adam(lr=self.lr_final), loss='mse')
+
             current_p = self.p_final
             print('Current p:', current_p)
-            bar = trange(self.p_level_batches)
+            bar = trange(self.final_batches)
             for i in bar:
                 self.pred_loss_train.append(self.train_pred_ae(train_getter, p=current_p))
                 smooth_loss = np.mean(self.pred_loss_train[-10:])
@@ -254,7 +283,91 @@ class HydraNet(object):
                     postfix['L test'] = smooth_loss
 
                 bar.set_postfix(**postfix)
-        self.save_modules()
+
+            self.save_modules()
+
+# ------------------------- DISPLAYING --------------------------------
+
+    def plot_losses(self, tag=0):
+        plt.plot(self.pred_loss_train)
+        batches = np.arange(len(self.pred_loss_test)) * TEST_EVERY_N_BATCHES
+        plt.plot(self.pred_loss_train, batches)
+        plt.title('Loss')
+        plt.ylabel('loss')
+        plt.xlabel('updates')
+        plt.legend(['train', 'test'])
+        fpath = '{}/loss-{}.gif'.format(FOLDER_PLOTS, tag)
+        plt.savefig(fpath)
+
+    def draw_pred_gif(self, test_getter, p=1.0, use_pf=False, sim_config=None, use_stepper=False, tag=0):
+        episode = test_getter()
+        episode = episode[0, ...].reshape((1,) + episode.shape[1:])
+        episode_masked = self.mask_percepts(episode, p)
+        net_preds = self.pred_ae.predict(episode_masked[:, 0:-SERIES_SHIFT, ...])
+
+        # stepper predictions
+        stepper_pred = []
+        if use_stepper:
+            self.stepper.reset_states()
+            for t in range(EP_LEN-SERIES_SHIFT):
+                im = episode_masked[:, t, ...]
+                pred = self.stepper.predict(im)
+                stepper_pred.append(self.stepper.predict(im))
+
+        pf_pred = []
+        if use_pf:
+            pf = ParticleFilter(sim_config)
+            for t in range(EP_LEN-SERIES_SHIFT):
+                im = episode_masked[:, t, ...]
+                pass
+
+        # create header with labels
+        col = np.zeros((HEADER_HEIGHT, 1))
+
+        labels = []
+        labels.append(create_im_label('GT'))
+        labels.append(create_im_label('AE'))
+        if use_stepper:
+            labels.append(create_im_label('ST'))
+        if use_pf:
+            labels.append(create_im_label('PF'))
+
+        header = [col]
+        for label in labels:
+            header.append(label)
+            header.append(col)
+
+        header = np.concatenate(header, axis=1)
+
+        # combine predictions
+        col = np.ones((IM_HEIGHT, 1))
+        frames = []
+        for t in range(EP_LEN - SERIES_SHIFT):
+            images = []
+            images.append(episode[0, t+SERIES_SHIFT, :, :, 0])
+            images.append(net_preds[0, t, :, :, 0])
+            if use_stepper:
+                images.append(stepper_pred[t][0, :, :, 0])
+
+            if use_pf:
+                images.append(pf_pred[t])
+
+            table = [col]
+            for image in images:
+                table.append(image)
+                table.append(col)
+
+            frame = np.concatenate(table, axis=1)
+
+            # print(frame.shape)
+            width = frame.shape[1]
+            row = np.ones((1, width))
+            frame = np.concatenate([header, frame, row], axis=0)
+
+            frames.append(frame)
+
+        fpath = '{}/predictions-{}.gif'.format(FOLDER_PLOTS, tag)
+        imageio.mimsave(fpath, frames)
 
 
 if __name__ == '__main__':
@@ -264,7 +377,7 @@ if __name__ == '__main__':
     test_box.populate_images()
 
     hydra = HydraNet()
-    hydra.load_modules()
+    # hydra.load_modules()
     hydra.execute_scheme(train_box.get_batch_episodes, test_box.get_batch_episodes)
 
 

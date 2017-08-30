18854f899a7752c005df56f0c940622e735f37a9
Marian Andrecki <marian.andrecki@gmail.com>
2017-08-30 10:37:12 +0100

    new file for experiments

--- experiment.py
+++ experiment.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
+import datetime
+
 from balls_sim import DEFAULT_CONFIG
 from hydranet import HydraNet, DEFAULT_SCHEME
 
+
+class Experiment(object):
+    def __init__(self, ctrl_var):
+        self.date = datetime.datetime.now().strftime('%y-%m-%d_%H:%M')
+        self.ctrl_var = ctrl_var
 
